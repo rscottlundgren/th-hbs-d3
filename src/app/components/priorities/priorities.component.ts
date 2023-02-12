2fa4c2cac742ffc487b446afad1762022e538274
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-02-12 18:40:10 -0500

    Add `PrioritiesComponent`

---
+++ src/app/components/priorities/priorities.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-priorities',
+  templateUrl: './priorities.component.html',
+  styleUrls: ['./priorities.component.scss']
+})
+export class PrioritiesComponent {
+
+}
