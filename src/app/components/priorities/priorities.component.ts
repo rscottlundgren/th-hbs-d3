3e22490aaff59b7adf68711d6de425daba9eab03
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-02-13 11:02:15 -0500

    Update `PrioritiesComponent`

--- src/app/components/priorities/priorities.component.ts
+++ src/app/components/priorities/priorities.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Subscription } from 'rxjs';
+import { DATA } from '../../../db/data';
 
 @Component({
   selector: 'app-priorities',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./priorities.component.scss']
 })
 export class PrioritiesComponent {
+  prioritiesByTimeframe: any = [];
+  timeframe: any = {
+    daily: false,
+    weekly: true,
+    monthly: false
+  }
+  subscription: Subscription;
+
+  constructor(private dataService: DataService) {
+    this.subscription = this.dataService.getDataOnLinkClick().subscribe((value) => (this.timeframe = value));
+  }
+
+  ngOnInit(): void {
+    this.prioritiesByTimeframe = DATA;
+  }
 
 }
