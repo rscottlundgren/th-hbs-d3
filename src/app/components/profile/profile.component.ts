f87c61554f0ed615d44973226a713472fe4d200b
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-02-13 11:02:38 -0500

    Update `ProfileComponent`

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { DataService } from '../../services/data.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +10,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  @Input() isDailyActive: boolean = false;
+  @Input() isWeeklyActive: boolean = true;
+  @Input() isMonthlyActive: boolean = false;
+  subscription: Subscription;
 
+  constructor(private uiService: UiService, private dataService: DataService) {
+    this.subscription = this.uiService.onDailyToggle().subscribe((value) => (this.isDailyActive = value));
+    this.subscription = this.uiService.onWeeklyToggle().subscribe((value) => (this.isWeeklyActive = value));
+    this.subscription = this.uiService.onMonthlyToggle().subscribe((value) => (this.isMonthlyActive = value));
+  }
+
+  ngOnInit() { }
+
+  onDailyClick() {
+    this.uiService.toggleDailyActive();
+    if (this.isWeeklyActive === true) {
+      this.uiService.toggleWeeklyActive();
+    }
+    if (this.isMonthlyActive === true) {
+      this.uiService.toggleMonthlyActive();
+    }
+    this.dataService.toggleDailyActive();
+  }
+
+  onWeeklyClick() {
+    if (this.isDailyActive === true) {
+      this.uiService.toggleDailyActive();
+    }
+    this.uiService.toggleWeeklyActive();
+    if (this.isMonthlyActive === true) {
+      this.uiService.toggleMonthlyActive();
+    }
+    this.dataService.toggleWeeklyActive();
+  }
+
+  onMonthlyClick() {
+    if (this.isDailyActive === true) {
+      this.uiService.toggleDailyActive();
+    }
+    if (this.isWeeklyActive === true) {
+      this.uiService.toggleWeeklyActive();
+    }
+    this.uiService.toggleMonthlyActive();
+    this.dataService.toggleMonthlyActive();
+  }
 }
